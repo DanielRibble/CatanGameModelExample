cf240fec271b6bcce6d018f3ded927f4f97574e5
Daniel Stefan Ribble <dsribble@longbow.cs.byu.edu>
2016-02-10 09:28:42 -0700

    Catan GameModel Example

---
+++ MessageLine.java
@@ -0,0 +1,62 @@
+package shared.gameModel;
+
+import java.io.Serializable;
+
+/**
+ * This class represents a message line.
+ * @author Ife's group
+ *
+ */
+public class MessageLine implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2339467380520161482L;
+	private String message;
+	private String source;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((source == null) ? 0 : source.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MessageLine other = (MessageLine) obj;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (source == null) {
+			if (other.source != null)
+				return false;
+		} else if (!source.equals(other.source))
+			return false;
+		return true;
+	}
+	
+	
+}
